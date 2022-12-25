910f31a36dd401b355011f7d6f62362fce8e597f
Daliaatef <93462059+Daliaatef@users.noreply.github.com>
2022-12-25 09:29:12 +0200

    Create  firstpython.py

---
+++ firstpython.py
@@ -0,0 +1,9 @@
+ My Jupyter Notebook On IBM Waston Studio
+Dalia Atef
+my desire occupation is data analyst
+i am interesting in data science because it helping businesses make better decisions
+my code is for write hello python
+print("hello python")
+hello python
+(***)
+[https://markdown-guide.readthedocs.io/en/latest/basics.html#links-inline]
